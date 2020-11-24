56325af5bd8cc98f22cb2d474a0cdc2734afb5b4
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-11-24 22:39:42 +0530

    checkpoint

---
+++ .gitignore/api/apiKey.py
@@ -0,0 +1,1 @@
+api_key = 'NzgwNjg2MTE2MjQzNjM2MjY1.X7ysmg.QL_V0KM7-KLwZbHCiDoWgrj-57U'
--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import discord
+import random
+from api import apiKey
 from discord.ext import commands
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
@@ -8,6 +10,8 @@ client = commands.Bot(command_prefix = '.', intents = intents)
 async def on_ready() :
     print(f'{client.user} is ready')
 
+
+#MEMBER JOIN AND LEAVE
 @client.event
 async def on_member_join(member) :
     print(f'{member} has joined a server')
@@ -15,5 +19,35 @@ async def on_member_join(member) :
 @client.event
 async def on_member_remove(member) :
     print(f'{member} has left the server')
+#MEMBER JOIN AND LEAVE
 
-client.run('NzgwNjg2MTE2MjQzNjM2MjY1.X7ysmg.R4RqDcP2fPqnUUaojf7xLjLlT8o')
+
+@client.command()
+async def ping(ctx) :
+    pings = [
+        'Ki hoyeche bhai kono kaaj nei naki?',
+        'Pong!',
+        'Ayo whaddup',
+        'Humko Disturb mat kario'
+    ]
+    await ctx.send(random.choice(pings))
+
+
+@client.command(aliases = ['spam', 'test'])
+async def _spam(ctx, user: discord.Member, num) :
+    for i in range(int(num)) :
+        await ctx.send(f'{user.mention}')
+
+
+@client.command(aliases = ['future'])
+async def _future(ctx, *, member) :
+    futures = ['Potty porishkar wala',
+               'Harshad Mehta babua',
+               'Kyalane',
+               'Pro Google coder pola',
+               "Xyber's subordinate"]
+    await ctx.send(f'{member} will be {random.choice(futures)} in the future')
+
+
+
+client.run(apiKey.api_key)
